b1b967d86a6593cd77df56fe59ecf34402ed2506
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-05-28 17:54:18 +0300

    forEach with arrays

--- script.js
+++ script.js
@@ -1,48 +1,82 @@
 'use strict';
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['CNY', 'Chinese yuan'],
-]);
+//const currencies = new Map([
+//  ['USD', 'United States dollar'],
+//  ['EUR', 'Euro'],
+//  ['CNY', 'Chinese yuan'],
+//]);
 
-const transactions = [300, 250, -500, 5000, -750, -180, 50, 1400, -350, -120];
+////const transactions = [300, 250, -500, 5000, -750, -180, 50, 1400, -350, -120];
 
-//////////////////////////////////////////////
-// Простые Методы Массивов
+////////////////////////////////////////////////
+//// Простые Методы Массивов
 
-// let letters = ['a', 'b', 'c', 'd', 'e', 'f'];
+//// let letters = ['a', 'b', 'c', 'd', 'e', 'f'];
 
-slice();
+//slice();
 
-console.log(letters.slice(1));
-console.log(letters.slice(1, 4));
-console.log(letters.slice(-1));
-console.log(letters.slice(-3));
-console.log(letters.slice(-3, -1));
-console.log(letters.slice(2, -1));
-console.log(letters.slice());
-console.log([...letters]);
+//console.log(letters.slice(1));
+//console.log(letters.slice(1, 4));
+//console.log(letters.slice(-1));
+//console.log(letters.slice(-3));
+//console.log(letters.slice(-3, -1));
+//console.log(letters.slice(2, -1));
+//console.log(letters.slice());
+//console.log([...letters]);
 
-splice();
+//splice();
 
-console.log(letters.splice(1));
-letters.splice(-1);
-letters.splice(1, 3);
+//console.log(letters.splice(1));
+//letters.splice(-1);
+//letters.splice(1, 3);
 
-let letters1 = ['a', 'b', 'c', 'd', 'e', 'f'];
-let letters2 = ['g', 'h', 'i'];
+//let letters1 = ['a', 'b', 'c', 'd', 'e', 'f'];
+//let letters2 = ['g', 'h', 'i'];
 
-reverse();
+//reverse();
 
-console.log(letters.reverse());
+//console.log(letters.reverse());
 
-concat();
+//concat();
 
-const letters = letters1.concat(letters2);
-console.log(letters);
-console.log([...letters1, ...letters2]);
+//const letters = letters1.concat(letters2);
+//console.log(letters);
+//console.log([...letters1, ...letters2]);
 
-join();
+//join();
 
-console.log(letters.join(', '));
+//console.log(letters.join(', '));
+
+// ForEach() _________________________________________________________
+
+// const transactions = [300, 250, -500, 5000, -750, -180, 50, 1400, -350, -120];
+
+// for (const transaction of transactions) {
+//   if (transaction > 0) {
+//     console.log(`${transaction} was deposited`);
+//   } else {
+//     console.log(`${Math.abs(transaction)} was withdrew`);
+//   }
+// }
+
+// for (const [i, transaction] of transactions.entries()) {
+//   if (transaction > 0) {
+//     console.log(`${transaction} was deposited`);
+//     console.log(i);
+//   } else {
+//     console.log(`${Math.abs(transaction)} was withdrew`);
+//     console.log(i);
+//   }
+// }
+
+// console.log('------------------------');
+
+// transactions.forEach(function (transaction, index /*, array*/) {
+//   if (transaction > 0) {
+//     console.log(`Transaction № ${index + 1}: ${transaction} was deposited`);
+//   } else {
+//     console.log(
+//       `Transaction № ${index + 1}: ${Math.abs(transaction)} was withdrew`
+//     );
+//   }
+// });
