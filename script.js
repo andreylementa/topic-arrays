05f41f081b29c2c1fc695533d25b089f642cb5b7
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-05-28 18:07:46 +0300

    forEach with Map and Set

--- script.js
+++ script.js
@@ -80,3 +80,25 @@
 //     );
 //   }
 // });
+
+// forEach С Map И Set _____________________________________________________________________
+
+// Map
+
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['CNY', 'Chinese yuan'],
+// ]);
+
+// currencies.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
+
+// // Set
+// const uniqueCurrencies = new Set(['USD', 'EUR', 'USD', 'CNY', 'CNY']);
+// console.log(uniqueCurrencies);
+// uniqueCurrencies.forEach(function (value, _, set) {
+//   console.log(`${value}: ${value}`);
+//   console.log(set);
+// });
