07efd6230e1e2b7fbedad8a8f19e0b49638fa7a3
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-06-01 16:13:29 +0300

    Array.fill() and Array.from()

--- script.js
+++ script.js
@@ -275,3 +275,31 @@
 // transactions.sort((x, y) => y - x);
 
 // console.log(transactions);
+
+////////////////////////////////////////////////
+// Другие Способы Создания И Заполнения Массивов
+
+// console.log([1, 2, 3]);
+// console.log(new Array(1, 2, 3));
+
+// // empty arrays
+// const arr = new Array(3);
+// console.log(arr);
+// // console.log(arr.map(() => 7));
+
+// // Method fill()
+// // arr.fill(7);
+// // arr.fill(7, 1);
+// arr.fill(7, 1, 2);
+// console.log(arr);
+
+// const arr1 = [1, 1, 1, 1, 1];
+// arr1.fill(3, 2, 4);
+// console.log(arr1);
+
+// Array.from() method
+// const arr = Array.from({ length: 5 }, () => 1);
+// console.log(arr);
+
+// const arr1 = Array.from({ length: 5 }, (_, index) => index + 1);
+// console.log(arr1);
