bd228eb3f8920b1b0c93d7ae11d0500941a89a73
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-06-02 14:15:59 +0300

    Arrays ex. 1,2

--- script.js
+++ script.js
@@ -303,3 +303,55 @@
 
 // const arr1 = Array.from({ length: 5 }, (_, index) => index + 1);
 // console.log(arr1);
+
+const account1 = {
+  userName: 'Cecil Ireland',
+  transactions: [500, 250, -300, 5000, -850, -110, -170, 1100],
+  interest: 1.5,
+  pin: 1111,
+};
+
+const account2 = {
+  userName: 'Amani Salt',
+  transactions: [2000, 6400, -1350, -70, -210, -2000, 5500, -30],
+  interest: 1.3,
+  pin: 2222,
+};
+
+const account3 = {
+  userName: 'Corey Martinez',
+  transactions: [900, -200, 280, 300, -200, 150, 1400, -400],
+  interest: 0.8,
+  pin: 3333,
+};
+
+const account4 = {
+  userName: 'Kamile Searle',
+  transactions: [530, 1300, 500, 40, 190],
+  interest: 1,
+  pin: 4444,
+};
+
+const account5 = {
+  userName: 'Oliver Avila',
+  transactions: [630, 800, 300, 50, 120],
+  interest: 1.1,
+  pin: 5555,
+};
+
+const accounts = [account1, account2, account3, account4, account5];
+
+// Ex. 1
+
+const bankDepositTotal = accounts
+  .flatMap(account => account.transactions)
+  .filter(trans => trans > 0)
+  .reduce((acc, trans) => (acc += trans), 0);
+console.log(bankDepositTotal);
+
+// Ex. 2
+
+const withdrawl = accounts
+  .flatMap(account => account.transactions)
+  .filter(widh => widh <= -500).length;
+console.log(withdrawl);
